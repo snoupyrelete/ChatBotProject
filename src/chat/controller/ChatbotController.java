2f8bbfd9a88e3c72f0f35eed94fb6b57e314637a
snoupyrelete <drob8896@740s-pl-112.local>
2016-10-18 08:52:26 -0600

    Added while test run loop in start of chatbot controller

--- src/chat/controller/ChatbotController.java
+++ src/chat/controller/ChatbotController.java
@@ -1,9 +1,26 @@
 package chat.controller;
 
+import chat.model.Chatbot;
+import chat.view.ChatbotViewer;
+
 public class ChatbotController
 {
+	private Chatbot stupidBot;
+	private ChatbotViewer display;
+	
+	public ChatbotController()
+	{
+		stupidBot = new Chatbot("ROBO-BOT-AUTONOMOUS-MECHANICAL-AI-LEARNING-HUMAN-TALK-MACHINE-TO-ACQUIRE-CURRENCY-AND-HUMAN-FEMALES");
+		display = new ChatbotViewer();
+	}
+	
 	public void start() 
 	{
+		String response = "words";
+		while (stupidBot.lengthChecker(response))
+		{
+			response = display.collectResponse("Give me something baby");
+		}
 		
 	}
 }
