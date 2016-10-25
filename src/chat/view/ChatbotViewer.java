3d4d7a053b52e36a5287e13db233970f096b7279
snoupyrelete <drob8896@740s-pl-129.local>
2016-10-25 08:12:06 -0600

    Necessary commit

--- src/chat/view/ChatbotViewer.java
+++ src/chat/view/ChatbotViewer.java
@@ -1,9 +1,17 @@
 package chat.view;
 
 import javax.swing.JOptionPane;
-
+import javax.swing.ImageIcon;
 public class ChatbotViewer
 {
+	private String windowMessage;
+	private ImageIcon ChatIcon;
+	
+	public ChatbotViewer()
+	{
+		windowMessage = "This message is from chatbot!";
+		ChatIcon = new ImageIcon(getClass().getResource("images/chatbot.png"));
+	}
 	public String collectResponse(String question) 
 	{
 		String response = "";
